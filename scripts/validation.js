7bb00a9f79de502c6da29b0d072fb31707232aa8
joseiddd <joseidd@outlook.com>
2024-10-26 08:57:43 -0400

    new update

--- scripts/validation.js
+++ scripts/validation.js
@@ -43,7 +43,7 @@ const toggleButtonState = (inputList, buttonEl, config) => {
 };
 
 const disableButton = (buttonEl, config) => {
-  buttonEl.disable = true;
+  buttonEl.disabled = true;
   buttonEl.classList.add(config.inactiveButtonClass);
 };
 
